98ebaa9af608fcfc1370d1b6a7bc4dc0568e203d
sahayraghav <97756324+sahayraghav@users.noreply.github.com>
2024-01-20 20:26:49 +0530

    removed raghav smart hai

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const Task = ({ title, description, deleteTask, index }) => {
   return (
     <>
-    <p>raghav smart hai</p>
     <div className="task">
       <div>
         <p>{title}</p>
